6b7b0487167d3f6435632a50f792911c1c31669f
Sushil Kandu <127728406+Susilkandu@users.noreply.github.com>
2024-09-26 23:10:09 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const sessionSecret = "type here your session secret key";
-const email='type here your email id for the email sending';
-const emailPassword='email id  password for application';
+const email='youfriendy324@gmail.com';
+const emailPassword='vwfblhrdrqxwksar';
 
 module.exports ={
     sessionSecret,
